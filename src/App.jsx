d381875dc4afbb634951ab32d7850193b0d6fb53
Fay Lee <fay.elizabeth.lee@gmail.com>
2025-05-21 15:50:20 -0400

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { endpoints } from './constants/endpoints';
+
 import About from './components/About';
 import HowIWork from './components/HowIWork';
 import CaseStudy1 from './components/CaseStudy1';
@@ -10,11 +12,11 @@ import CaseStudy3 from './components/CaseStudy3';
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<About />} />
-      <Route path="/how-i-work" element={<HowIWork />} />
-      <Route path="/case-study-1" element={<CaseStudy1 />} />
-      <Route path="/case-study-2" element={<CaseStudy2 />} />
-      <Route path="/case-study-3" element={<CaseStudy3 />} />
+      <Route path="/" element={<About header="About Me" />} />
+      <Route path="/how-i-work" element={<HowIWork header="How I Work" />} />
+      <Route path="/case-study-1" element={<CaseStudy1 header="Case Study 1" />} />
+      <Route path="/case-study-2" element={<CaseStudy2 header="Case Study 2" />} />
+      <Route path="/case-study-3" element={<CaseStudy3 header="Case Study 3" />} />
     </Routes>
   </Router>
 );
