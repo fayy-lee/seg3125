6bfadc5e5e5dd35796ede076594c4a6dc662ec3b
Fay Lee <fay.elizabeth.lee@gmail.com>
2025-05-21 14:53:08 -0400

    fixing

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,13 +1,11 @@
 const endpoints = {
-  navbar: 'profile/navbar.json',
-  routes: 'profile/routes.json',
-  home: 'profile/home.json',
-  social: 'profile/social.json',
-  about: 'profile/about.json',
-  howiwork: 'profile/howiwork.json',
-  casestudy1: 'profile/casestudy1.json',
-  casestudy2: 'profile/casestudy2.json',
-  casestudy3: 'profile/casestudy3.json',
+  navbar: '/profile/navbar.json',
+  routes: '/profile/routes.json',
+  home: '/profile/home.json',
+  social: '/profile/social.json',
+  about: '/profile/about.json',
+  howiwork: '/profile/howiwork.json',
+  casestudy1: '/profile/casestudy1.json',
+  casestudy2: '/profile/casestudy2.json',
+  casestudy3: '/profile/casestudy3.json',
 };
-
-export default endpoints;
