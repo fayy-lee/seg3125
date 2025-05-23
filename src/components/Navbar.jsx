5ff0febe838ab3289dd0b4e66af3f1bee63c165c
Fay Lee <fay.elizabeth.lee@gmail.com>
2025-05-22 20:14:51 -0400

    nav bar fixing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,33 +4,35 @@ import { NavLink } from 'react-router-dom';
 const Navbar = ({ darkMode, setDarkMode }) => {
   return (
     <nav className="navbar">
-      <div className="navbar-left">
-        <img src="/images/logo.png" alt="Logo" className="logo" />
-      </div>
-      <div className="navbar-right">
-        <NavLink to="/" className="nav-link" end>
-          Home
-        </NavLink>
-        <NavLink to="/about" className="nav-link">
-          About
-        </NavLink>
-        <NavLink to="/how-i-work" className="nav-link">
-          How I Work
-        </NavLink>
-        <NavLink to="/case-studies" className="nav-link">
-          Case Studies
-        </NavLink>
+        <div className="navbar-content">
+            <div className="navbar-left">
+                <img src="/images/logo.png" alt="Logo" className="logo" />
+            </div>
+            <div className="navbar-right">
+                <NavLink to="/" className="nav-link" end>
+                Home
+                </NavLink>
+                <NavLink to="/about" className="nav-link">
+                About
+                </NavLink>
+                <NavLink to="/how-i-work" className="nav-link">
+                How I Work
+                </NavLink>
+                <NavLink to="/case-studies" className="nav-link">
+                Case Studies
+                </NavLink>
 
-        {/* Dark Mode Toggle */}
-        <label className="toggle-switch">
-          <input
-            type="checkbox"
-            checked={darkMode}
-            onChange={() => setDarkMode(!darkMode)}
-          />
-          <span className="slider"></span>
-        </label>
-      </div>
+                {/* Dark Mode Toggle */}
+                <label className="toggle-switch">
+                <input
+                    type="checkbox"
+                    checked={darkMode}
+                    onChange={() => setDarkMode(!darkMode)}
+                />
+                <span className="slider"></span>
+                </label>
+            </div>
+        </div>
     </nav>
   );
 };
